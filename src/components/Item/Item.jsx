3393f81f5d7615060842b604529ce8cf769c4148
rasel754 <raselahmed73614@gmail.com>
2024-03-17 04:05:29 +0600

    add item dynamicly

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -4,9 +4,9 @@ const Item = ({item}) => {
     const {recipe_image,recipe_name,short_description ,ingredients ,preparing_time ,calories ,} = item;
     return (
         <div>
-            <div className="bg-[#878787] rounded-2xl">
-            <img className='w-[400px] h-[220px] p-6' src={recipe_image} alt="" />
-            <h1>{recipe_name}</h1>
+            <div className="bg-[#878787] rounded-2xl px-6">
+            <img className='rounded-3xl w-[400px] h-[220px]  ' src={recipe_image} alt="" />
+            <h1 className="text-2xl font-semibold text-black "> {recipe_name}</h1>
             <p>{short_description}</p>
             <h2>Ingredients: 6</h2>
             <ol title="Ingredients: 6">
@@ -15,7 +15,10 @@ const Item = ({item}) => {
                 ))}
             </ol>
             <p><img src="../../../public/image/frame1.png" alt="" /> <span>{preparing_time}</span></p>
-            <p><img src="../../../public/image/frame2.png" alt="" /> <span>{calories}</span></p>       
+            <p><img src="../../../public/image/frame2.png" alt="" /> <span>{calories}</span></p>   
+            <button className="btn btn-success">Want to Cook</button>
+
+            
         </div>
         </div>
     );
